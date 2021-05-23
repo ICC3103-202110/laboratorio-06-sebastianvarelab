b875d2ea915d647fa255eab669370ca4efc2bef2
sebastianvarelab <svarela@miuandes.cl>
2021-05-23 12:32:07 -0400

    model commit

---
+++ model.js
@@ -0,0 +1,14 @@
+initModel = {
+    left_temperature: "Y/n",
+    temperature_value_convert: 0,
+    from: 0,
+    to: 0,
+    leftValue: 0,
+    leftUnit: "Celsius",
+    rightValue: 32,
+    rightUnit: "Fahrenheit",
+}
+
+module.exports = {
+    initModel
+}
